44ecf8e9c6b251eac1e7e7f22d42ea4426f30b10
anishav109-lgtm <anishav109@gmail.com>
2026-09-21 10:38:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-python
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
@@ -89,4 +88,3 @@ def predict():
 # Run application
 if __name__ == "__main__":
     app.run(debug=True)
-
